29877f4f66955e791719687557b119e3b9cf829f
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2024-04-06 02:57:02 +0800

    Refactor server.ts to dynamically handle spin_up and spin_down routes based on environment

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
+import 'dotenv/config';
 import express from 'express';
+import {dev_spin_up, dev_spin_down} from './dev_server';
+import {prd_spin_up, prd_spin_down} from './prd_server';
 
 const server = express();
 
-server.get('/spin_up', (req, res) => {
-  console.log('Spinning up a container');
-  res.send('Container is spinning up');
-});
+if (process.env.NODE_ENV === 'dev') {
+  server.get('/spin_up', dev_spin_up);
+  server.get('/spin_down', dev_spin_down);
+} else {
+  server.get('/spin_up', prd_spin_up);
+  server.get('/spin_down', prd_spin_down);
+}
 
-server.get('/spin_down', (req, res) => {
-  console.log('Spinning down a container');
-  res.send('Container is spinning down');
+const port = process.env.SERVER_PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Environment:', process.env.NODE_ENV);
+  console.log(`Server is listening on ${port}`);
 });
 
 export default server;
--- src/startup.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import server from './server';
-
-const port = process.env.SERVER_PORT || 3000;
-
-server.listen(port, () => {
-  console.log('Environment:', process.env.NODE_ENV);
-  console.log(`Server is listening on ${port}`);
-});
